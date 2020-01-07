753fd82795de2a7a581135b37d4366c1d18e7985
chen <chen_andrew@berkeley.edu>
2020-01-07 00:04:42 -0800

    Switch canvas to table to allow for shrinking cells

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,25 @@
 allCanvasBlocks = []
-const CANVAS_SIZE = 16;
+const CANVAS_SIZE = 40;
 function createCanvasBlock() {
-    let canvasBlock = document.createElement("div");
-    canvasBlock.textContent = "test";
+    let canvasBlock = document.createElement("td");
     canvasBlock.classList.add("canvas-box");
     return canvasBlock;
 }
 function createCanvas() {
     let canvas = document.getElementById("canvas");
+    let row, canvasBlock;
     for (let i = 0; i < CANVAS_SIZE; ++i) {
+        row = document.createElement("tr");
         for (let j = 0; j < CANVAS_SIZE; ++j) {
-            let canvasBlock = createCanvasBlock();
-            canvas.appendChild(canvasBlock);
+            canvasBlock = createCanvasBlock();
+            row.appendChild(canvasBlock);
             allCanvasBlocks.push(canvasBlock);
         }
+        canvas.appendChild(row);
     }
-    canvas.style.gridTemplateRows = `repeat(${CANVAS_SIZE}, 1fr)`;
-    canvas.style.gridTemplateColumns = `repeat(${CANVAS_SIZE}, 1fr)`;
+    // }
+    // canvas.style.gridTemplateRows = `repeat(auto-fit, ${CANVAS_SIZE}, 1fr)`;
+    // canvas.style.gridTemplateColumns = `repeat(auto-fit, ${CANVAS_SIZE}, 1fr)`;
     return canvas; 
 }
 const CANVAS = createCanvas();
